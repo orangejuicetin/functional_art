8e940099cf922b93f05bcef303f48d4fbaa94876
Justin Choi <justinchoi2000@gmail.com>
2023-10-03 18:51:08 -0400

    ALEX CHEN THE GOAT

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,56 @@
 module Main (main) where
 
-import Lib
+import Control.Monad.Reader
+import Control.Monad.State
+import Graphics.Rendering.Cairo
+import System.Random
+
+data World = World
+  { worldWidth :: Double,
+    worldHeight :: Double,
+    scaleFactor :: Double
+  }
+
+type Generate a = StateT StdGen (Reader World) a
+
+runGenerate :: World -> StdGen -> Generate a -> a
+runGenerate world rng scene =
+  flip runReader world . fmap fst . flip runStateT rng $ scene
+
+bg :: Generate (Render ())
+bg = do
+  (World w h _) <- ask
+  return $ do
+    setSourceRGBA 0 0 0 1
+    rectangle 0 0 w h
+    fill
+
+draw :: Generate (Render ())
+draw = do
+  (World w h _) <- ask
+  red <- state $ uniformR (0, 1)
+  green <- state $ uniformR (0, 1)
+  blue <- state $ uniformR (0, 1)
+  return $ do
+    setSourceRGBA red green blue 1
+    rectangle (w / 4) (h / 4) (w / 2) (h / 2)
+    fill
+
+sketch :: Generate (Render ())
+sketch = do
+  rs <- sequence [bg, draw]
+  return $ sequence_ rs
 
 main :: IO ()
-main = someFunc
+main = do
+  let world = World 500 200 1
+  rng <- initStdGen
+  surface <-
+    createImageSurface
+      FormatARGB32
+      (round $ worldWidth world)
+      (round $ worldHeight world)
+  renderWith surface $ do
+    scale (scaleFactor world) (scaleFactor world)
+    runGenerate world rng sketch
+  surfaceWriteToPNG surface "out.png"
